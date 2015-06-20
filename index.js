0c37db8d591e18844f671b2451b1925b8c0462e9
tunnckoCore <mameto_100@mail.bg>
2015-06-20 20:43:32 +0300

    lets get started

--- index.js
+++ index.js
@@ -7,6 +7,32 @@
 
 'use strict'
 
-module.exports = function expressIpFilter () {
-  // body
+var ipFilter = require('ip-filter')
+
+module.exports = function expressIpFilter (options) {
+  options = typeof options === 'object' ? options : {}
+
+  return function (req, res, next) {
+    var id = typeof options.id === 'function' ? options.id.call(this, req, res) : req.ip
+
+    if (!id || !options.filter) {
+      next()
+      return
+    }
+
+    var strict = typeof options.strict === 'boolean' ? options.strict : true
+    var forbidden = options.forbidden || '403 Forbidden'
+
+    var identifier = ipFilter(id, options.filter, !strict)
+    if (identifier === null) {
+      var body = typeof forbidden === 'function' ? forbidden.call(this, req, res) : forbidden
+      res.status(403).send(body)
+      return
+    }
+
+    res.filter = ipFilter
+    res.identifier = identifier
+
+    next()
+  }
 }
